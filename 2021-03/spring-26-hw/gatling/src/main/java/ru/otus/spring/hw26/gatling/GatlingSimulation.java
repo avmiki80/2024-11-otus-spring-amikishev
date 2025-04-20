1b80bb147ae3d83fa2c6cc83aab21d968edc5514
avm <avmiki80@mail.ru>
2025-04-20 21:32:02 +0500

    ДЗ 27

---
+++ 2021-03/spring-26-hw/gatling/src/main/java/ru/otus/spring/hw26/gatling/GatlingSimulation.java
@@ -0,0 +1,68 @@
+package ru.otus.spring.hw26.gatling;
+
+import io.gatling.javaapi.core.ChainBuilder;
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
+
+import java.time.Duration;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.http;
+
+
+public class GatlingSimulation extends Simulation {
+
+    // 1. Определение HTTP протокола
+    HttpProtocolBuilder httpProtocol = http
+            .baseUrl("http://localhost:8080")
+            .acceptHeader("application/json")
+            .contentTypeHeader("application/json")
+            .authorizationHeader("Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidXNlcm5hbWUiOiJhZG1pbiIsInJvbGVzIjpbIkFETUlOIl0sImV4dCI6MjAxNjIzOTAyMiwiaWF0IjoxNTE2MjM5MDIyfQ.QqUWw8a3AbmX6lRg7xl2Ks3WMk7ORurS-M-qjiiTTdDB-zOK7_-RRTavgPBXNHjq5jVsC6U29fY6RRFh3YP3SA");
+
+    // Генерация JSON-массива коментариев
+    ChainBuilder generateComments =
+            exec(session -> {
+                String commentsJson = IntStream.rangeClosed(1, 1000)
+                        .mapToObj(i -> String.format(
+                                "{\"text\":\"Comment%d\",\"bookTitle\":\"Book%d\"}", i, i%3))
+                        .collect(Collectors.joining(",", "[", "]"));
+                return session.set("commentsJson", commentsJson);
+            });
+
+    // 2. Определение сценария
+    ScenarioBuilder comments =
+            scenario("Mass create comments loading")
+                    .exec(generateComments)
+//                    .exec(
+//                            http("Find comment by id")
+//                                    .get("/comment/1")
+//                                    .header("content-type", "application/json; charset=utf-8")
+//                                    .requestTimeout(1200))
+                    .exec(
+                            http("Mass create comments")
+                                    .post("/comments")
+                                    .header("content-type", "application/json; charset=utf-8")
+                                    .requestTimeout(1200)
+                                    .body(StringBody("#{commentsJson}")));
+//                    .exec(
+//                            http("Find comment by id")
+//                                    .get("/comment/2")
+//                                    .header("content-type", "application/json; charset=utf-8")
+//                                    .requestTimeout(1200));
+
+    // 3. Настройка нагрузки
+    {
+        setUp(
+                comments.injectOpen(
+                        nothingFor(5), // Ничего не делать 5 секунд
+                        rampUsers(5).during(10), // Линейно увеличивать до 5 запросов за 10 секунд
+                        constantUsersPerSec(2).during(30), // Поддерживать 2 запросов/сек в течение 30 секунд
+                        rampUsersPerSec(1).to(5).during(20) // Увеличивать с 1 до 5 запросов/сек за 20 секунд
+                )
+        ).protocols(httpProtocol)
+                .maxDuration(Duration.ofMinutes(5));
+    }
+}
---
+++ 2021-03/spring-26-hw/moderator/src/main/java/ru/otus/spring/hw26/moderator/ModeratorApplication.java
@@ -0,0 +1,18 @@
+package ru.otus.spring.hw26.moderator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+
+@SpringBootApplication
+@IntegrationComponentScan({
+        "ru.otus.spring.hw26.moderator.event.moderate.publish"
+})
+public class ModeratorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ModeratorApplication.class, args);
+        System.out.println("KAFKA_URL from env: " + System.getenv("KAFKA_URL"));
+    }
+
+}
