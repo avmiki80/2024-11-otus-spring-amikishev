41890536a96cc4bfc5c3c619a2d150ef0ebc8ac7
avm <avmiki80@mail.ru>
2025-04-20 21:55:53 +0500

    ДЗ 27

--- 2021-03/spring-25-hw/book-service/src/main/java/ru/otus/spring/book/service/ModeratorService.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.spring.book.service;
-
-import ru.otus.spring.book.dto.CommentDto;
-import ru.otus.spring.book.dto.ModerateDto;
-
-public interface ModeratorService {
-    ModerateDto toModerate(CommentDto commentDto);
-}
